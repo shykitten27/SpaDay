c64df5853780fcee80a7a750ecb081bdc60dd861
Donna Seidel <dseidel01@gmail.com>
2021-09-10 00:59:53 -0500

    start studio

---
+++ SpaDay/Controllers/UserController.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using SpaDay.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SpaDay.Controllers
+{
+    public class UserController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [Route("spa/user/add")]
+        public IActionResult Add()
+        {
+            return View();
+        }
+
+        public IActionResult SubmitAddUserForm(User newUser, string verify)
+        {
+            // add form submission handling code here
+            if (ViewBag.password == ViewBag.verify)
+            {
+                ViewBag.username = newUser;
+                return View("Index");
+            }
+            else 
+            {
+                return View();
+            }
+        }
+    }
+}
---
+++ SpaDay/Models/User.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SpaDay.Models
+{
+    public class User
+    {
+        public string Name { get; set; }
+        public string Username { get; set; }
+        public string Email { get; set; }
+        public string Password { get; set; }
+        public DateTime DateJoined { get; }
+
+        public User(string name, string username, string email, string password)
+        {
+            Name = name;
+            Username = username;
+            Email = email;
+            Password = password;
+            DateJoined = DateTime.Now;
+        }
+    }
+}
